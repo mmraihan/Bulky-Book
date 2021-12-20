9c696cec0fba076ac843cc60d49524814cddafb3
mmraihan <mm_raihan@live.com>
2021-12-20 21:47:35 +0600

    Category Upsert (Both Create and Edit) Get Action Method

--- BulkyBook/BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,24 @@ namespace BulkyBook.Areas.Admin.Controllers
         public IActionResult Index()
         {          
             return View();
+        }
+
+        public IActionResult Upsert(int ? id)
+        {
+            Category category = new Category();
+            if (id==null)
+            {
+                //create
+                return View(category);
+            }
+            //this is for Edit
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+            
         }
 
 
