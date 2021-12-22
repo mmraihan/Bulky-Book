9b610a49e267df5c1fd3225e47e6b6d60a5ab3a7
mmraihan <mm_raihan@live.com>
2021-12-23 03:46:45 +0600

    Company and User: Added ApplicationUser (added additional column in Existing column at AspNetUser table)

--- BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
 
     }
 }
