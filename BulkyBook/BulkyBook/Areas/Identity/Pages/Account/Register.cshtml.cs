eb547d90643fdd043104f999f103142df1a94eea
mmraihan <mm_raihan@live.com>
2021-12-23 21:10:54 +0600

    REGISTRATION: Added more fields to Register.cshtml.cs

--- BulkyBook/BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBook/BulkyBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -60,6 +60,17 @@ namespace BulkyBook.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string PostalCode { get; set; }
+            public string PhoneNumber { get; set; }
+            public int? CompanyId { get; set; } //Nullabe for individual users, They don't need any company
+            public string Role { get; set; }
+
         }
 
         public async Task OnGetAsync(string returnUrl = null)
