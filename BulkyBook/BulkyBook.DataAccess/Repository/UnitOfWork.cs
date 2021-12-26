27fbbe282b70fffadf5fc5af3ec0278352cebd2b
mmraihan <mm_raihan@live.com>
2021-12-27 01:01:21 +0600

    COMPLETED 15. Added all repositories and interface and UnitOfWork

--- BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -20,11 +20,17 @@ namespace BulkyBook.DataAccess.Repository
 
             Product = new ProductRepository(_db);
 
-            Company = new CompanyRepository(_db);
+            Company = new CompanyRepository(_db);   
+
+            SP_Call = new SP_Call(_db);
 
             ApplicationUser = new ApplicationUserRepository(_db);
 
-            SP_Call = new SP_Call(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+
+            OrderHeader = new OrderHeaderRepository(_db);
+
+            OrderDetails = new OrderDetailsRepository(_db);
         }
 
         
@@ -39,6 +45,10 @@ namespace BulkyBook.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
 
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailsRepository OrderDetails { get; private set; }
+        
 
         public void Dispose()
         {
