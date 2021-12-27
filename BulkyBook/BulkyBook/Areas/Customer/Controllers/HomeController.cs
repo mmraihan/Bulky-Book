773fa1df4432b0839c50944070cc8cb00d828600
mmraihan <mm_raihan@live.com>
2021-12-27 12:53:25 +0600

    Details&Shopping Cart: Add Product to shopping cart part-1

--- BulkyBook/BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -2,12 +2,14 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BulkyBook.Customer.Controllers
@@ -42,6 +44,53 @@ namespace BulkyBook.Customer.Controllers
                 ProductId=productFromDb.Id
             };
             return View(cartObj);
+
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart CartObject)
+        {
+            CartObject.Id = 0;
+            if (ModelState.IsValid)
+            {
+                //Theb we will add to cart
+
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                CartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                    u => u.ApplicationUserId == CartObject.ApplicationUserId && u.ProductId==CartObject.ProductId,
+                    includeProperties:("Product")
+                    );
+
+                if (cartFromDb==null)
+                {
+                    //No records exists in db for that user, we need to add product
+                }
+
+
+            }
+            else //Same prodcut will return back
+            {
+                var productFromDb = _unitOfWork.Product
+             .GetFirstOrDefault(u => u.Id == CartObject.ProductId, includeProperties: "Category,CoverType");
+
+
+                ShoppingCart cartObj = new ShoppingCart()
+                {
+                    Product = productFromDb,
+                    ProductId = productFromDb.Id
+                };
+                return View(cartObj);
+            }
+
+         
+           
 
         }
         public IActionResult Privacy()
